ae9d987608568fe9f3abf1c1e5e2ceb60f0ce2e9
Gabriel Fernandes <gabfeol@gmail.com>
2017-10-02 15:57:55 -0300

    letrec working

--- closureUntyped.rkt
+++ closureUntyped.rkt
@@ -6,17 +6,18 @@
 
 ; Expressões básicas
 (define-type ExprC
-  [numC  (n  number?)]
-  [idC   (s  symbol?)]
-  [plusC (l  ExprC?) (r  ExprC?)]
-  [multC (l  ExprC?) (r  ExprC?)]
-  [lamC  (arg  symbol?) (body  ExprC?)] ; nomes não são mais necessários
-  [appC  (fun  ExprC?) (arg  ExprC?)]
-  [ifC   (condição  ExprC?) (sim  ExprC?) (não  ExprC?)]
-  [quoteC (s symbol?)]
+  [numC    (n  number?)]
+  [idC     (s  symbol?)]
+  [plusC   (l  ExprC?) (r  ExprC?)]
+  [multC   (l  ExprC?) (r  ExprC?)]
+  [lamC    (arg  symbol?) (body  ExprC?)] ; nomes não são mais necessários
+  [appC    (fun  ExprC?) (arg  ExprC?)]
+  [ifC     (condição  ExprC?) (sim  ExprC?) (não  ExprC?)]
+  [quoteC  (s symbol?)]
+  [letrecC (s symbol?) (fun ExprC?) (arg ExprC?)] ; fat lamba (fat 5)
   )
 
-
+ 
 ; Expressões açucaradas
 (define-type ExprS
   [numS    (n  number?)]
@@ -29,6 +30,9 @@
   [multS   (l  ExprS?) (r  ExprS?)]
   [ifS     (c  ExprS?) (s  ExprS?) (n  ExprS?)]
   [quoteS  (s symbol?)]
+  [letS    (arg list?) (body ExprS?)]
+  [let*S   (arg list?) (body ExprS?)]
+  [letrecS (fun list?) (body ExprS?)] 
   )
 
 
@@ -45,6 +49,12 @@
     [uminusS (e)   (multC (numC -1) (desugar e))]
     [ifS     (c s n) (ifC (desugar c) (desugar s) (desugar n))]
     [quoteS  (s) (quoteC s)]
+    [letS    (arg body) (appC (lamC (first arg) (desugar body)) (desugar (second arg)))]
+    [let*S   (arg body) (appC (lamC (first (first arg))
+                                    (appC (lamC (first (second arg)) (desugar body)) (desugar (second (second arg)))))
+                              (desugar (second (first arg))))]
+    [letrecS (fun arg) (letrecC (first fun) (desugar (second fun)) (desugar arg))]
+                                    
     ))
 
 
@@ -63,7 +73,7 @@
 (define-type Value
   [numV  (n  number?)]
   [closV (arg  symbol?) (body  ExprC?) (env  list?)]
-  [symbolV (s symbol?)]) ;BOTEI SYMBOLV
+  [symbolV (s symbol?)])
 
 ; Novos operadores
 (define (num+ l r); Value x Value => Value
@@ -94,10 +104,17 @@
                         (bind (closV-arg f-value) (interp a env))
                         (closV-env f-value) ; não mais mt-env
                     )))]
+    
     [plusC (l r) (num+ (interp l env) (interp r env))]
     [multC (l r) (num* (interp l env) (interp r env))]
     [ifC (c s n) (if (zero? (numV-n (interp c env))) (interp n env) (interp s env))]
     [quoteC (s) (symbolV s)]
+    [letrecC (s f a)
+             (let ((func (interp f env)))
+               (begin
+                 (set-closV-env! func (extend-env (bind s func) env))
+                 (interp a (closV-env func))
+                 ))]
     ))
 
 ; Lookup para procurar símbolos no Environment
@@ -127,7 +144,14 @@
          [(call) (appS (parse (second sl)) (parse (third sl)))]
          [(if) (ifS (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
          [(quote) (quoteS (second sl))]
-         [(let) (appS (lamS (first (first (second sl))) (parse (third sl))) (parse (second (first (second sl)))))]
+         [(let) (letS (list (first (first (second sl))) (parse (second (first (second sl))))) (parse (third sl)))]
+         [(let*) (let*S (list (list (first (first (second sl))) (parse (second (first (second sl)))))
+                              (list (first (second (second sl))) (parse (second (second (second sl))))))
+                        (parse (third sl)))]
+         [(letrec) (letrecS (list (first (first (second sl))) (parse (second (first (second sl))))) (parse (third sl)))]
+         ;[(let*) (letS (list (first (first (second sl))) (parse (second (first (second sl)))))
+          ;             (letS (list (first (second (second sl))) (parse (second (second (second sl))))) (parse (third sl))))]
+         ;[(let) (appS (lamS (first (first (second sl))) (parse (third sl))) (parse (second (first (second sl)))))]
          [(load) (parse (first (let ((file (open-input-file (symbol->string (symbolV-s (interpS (second sl)))))))
                    (let f ((x (read file)))
                      (if (eof-object? x)
@@ -146,4 +170,24 @@
 (test (interp (plusC (numC 10) (appC (lamC '_ (numC 5)) (numC 10)))
               mt-env)
       (numV 15))
-(interpS '(+ 10 (call (func x (+ x x)) 16)))
+
+(test (interpS '(let ((x (* 2 3))) (+ x (+ x 7))))
+      (numV 19))
+
+
+(test (interpS '(let* ([x (* 2 3)] [y (+ 2 x)]) (- y x)))
+      (numV 2))
+
+(test (interpS '(+ 10 (call (func x (+ x x)) 16)))
+      (numV 42))
+
+(test (interpS '(letrec ([fac (lambda n (if n (* n (call fac (- n 1))) 1))]) (call fac 5)))
+      (numV 120))
+
+(test (interpS '(let ([x 2])
+            (let ([y 3])
+              (let ([foo (lambda z (+ x (+ y z)))])
+                (let ([x 7])
+                  (letrec ([bar (lambda k (if k (* 2 (call bar (- k 1))) 1))])
+                    (call bar (call foo 4))))))))
+      (numV 512))
